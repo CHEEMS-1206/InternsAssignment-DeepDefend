0c91983f983b2e60fec9b6707c307fab4c65e881
PRIYANSHU SINGH <sentysingh456@gmail.com>
2023-12-06 12:39:38 +0530

    rendered the sidebar and mainbody inside the app.js file

--- deep-defend-clientside/src/App.js
+++ deep-defend-clientside/src/App.js
@@ -1,9 +1,11 @@
+import Mainbody from "./Dashboard-1/Mainbody";
 import Sidebar from "./Dashboard-1/Sidebar";
 
 function App() {
   return (
     <div className="App">
-      <Sidebar/>
+      <Sidebar />
+      <Mainbody />
     </div>
   );
 }
